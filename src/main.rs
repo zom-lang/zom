e4e1f74660114a618f9cbc949ab792e320c8732a
Larsouille25 <superthithi2008@gmail.com>
2023-05-07 11:24:32 +0200

    Basic shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world from Mona!");
+    println!("Mona {}", env!("CARGO_PKG_VERSION"));
+
+    let mut input_buf = String::new();
+    loop {
+        print!("~>> ");
+        io::stdout().flush().unwrap();
+
+        match io::stdin().read_line(&mut input_buf) {
+            Ok(_len) => {}
+            Err(err) => {
+                println!("ERR: {}", err);
+                continue;
+            }
+        }
+        input_buf = String::from(input_buf.trim());
+
+        println!("input_buf = `{}`", input_buf);
+
+        if input_buf.starts_with("exit") {
+            println!("Goodbey !");
+            return;
+        }
+        input_buf = String::new();
+    }
 }
