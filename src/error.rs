618adae0e0af80a6e42ba3f64ab91b0b1884fe63
Larsouille25 <superthithi2008@gmail.com>
2023-05-17 13:30:23 +0200

    refacto of errors

--- src/error.rs
+++ src/error.rs
@@ -66,34 +66,48 @@ fn num_str_fix_len(num: u32, len: usize) -> String {
     num_str
 }
 
+pub fn print_error(f: &mut fmt::Formatter<'_>, position: &Position, kind: &ErrorKind, name: String, details: String) -> fmt::Result {
+    //TODO: Support error messages with line digits bigger than 5 characters.
+    writeln!(
+        f,
+        "Err: {:?}, in file `{}` at line {} :",
+        kind, position.filename, position.line
+    )
+    .unwrap();
+    writeln!(f, " ... |").unwrap();
+    writeln!(
+        f,
+        "{}| {}",
+        num_str_fix_len(position.line, 5),
+        position
+            .filetext
+            .split('\n')
+            .nth((position.line - 1) as usize)
+            .unwrap()
+    )
+    .unwrap();
+    writeln!(f, " ... | {}^", spaces(position.column as usize)).unwrap();
+    write!(
+        f,
+        "       {}{}",
+        spaces(position.column as usize),
+        name
+    ).unwrap();
+    if details.len() > 0 {
+        println!();
+        return write!(
+            f,
+            "        {}{}",
+            spaces(position.column as usize),
+            details
+        )
+    }
+    write!(f, "")
+}
+
 impl fmt::Display for IllegalCharError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        //TODO: Support error messages with line digits bigger than 5 characters.
-        writeln!(
-            f,
-            "Err: {:?}, in file `{}` at line {} :",
-            self.kind, self.position.filename, self.position.line
-        )
-        .unwrap();
-        writeln!(f, " ... |").unwrap();
-        writeln!(
-            f,
-            "{}| {}",
-            num_str_fix_len(self.position.line, 5),
-            self.position
-                .filetext
-                .split('\n')
-                .nth((self.position.line - 1) as usize)
-                .unwrap()
-        )
-        .unwrap();
-        writeln!(f, " ... | {}^", spaces(self.position.column as usize)).unwrap();
-        write!(
-            f,
-            "       {}{}",
-            spaces(self.position.column as usize),
-            self.name
-        )
+        print_error(f, &self.position, &self.kind, self.name.to_owned(), String::new())
     }
 }
 
@@ -126,38 +140,6 @@ impl Error for GeneralError {
 
 impl fmt::Display for GeneralError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        //TODO: Support error messages with line digits bigger than 5 characters.
-        writeln!(
-            f,
-            "Err: {:?}, in file `{}` at line {} :",
-            self.kind, self.position.filename, self.position.line
-        )
-        .unwrap();
-        writeln!(f, " ... |").unwrap();
-        writeln!(
-            f,
-            "{}| {}",
-            num_str_fix_len(self.position.line, 5),
-            self.position
-                .filetext
-                .split('\n')
-                .nth((self.position.line - 1) as usize)
-                .unwrap()
-        )
-        .unwrap();
-        writeln!(f, " ... | {}^", spaces(self.position.column as usize)).unwrap();
-        write!(
-            f,
-            "       {}{} :",
-            spaces(self.position.column as usize),
-            self.name
-        ).unwrap();
-        println!();
-        write!(
-            f,
-            "        {}{}",
-            spaces(self.position.column as usize),
-            self.details
-        )
+        print_error(f, &self.position, &self.kind, self.name.to_string(), self.details.to_string())
     }
 }
