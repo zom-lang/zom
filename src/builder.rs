9f6111d734fea1fc1789705db45a45835ed27f94
Larsouille25 <superthithi2008@gmail.com>
2023-05-25 20:44:25 +0200

    Creating GlobalContext struct

--- src/builder.rs
+++ src/builder.rs
@@ -1,27 +1,19 @@
-// // use std::collections::HashMap;
+// use std::collections::HashMap;
 
-// use inkwell::{context::Context as LLVMContext, builder::Builder, types::IntType};
+use inkwell::context::Context;
+use inkwell::builder::Builder;
 
-// pub struct Context<'a> {
-//     context: Box<LLVMContext>,
-//     builder: Builder<'a>,
-//     // named_values: HashMap<String, LLVMValueRef>,
-//     typ: IntType<'a>
-// }
+pub struct CodeGen<'ctx> {
+    context: &'ctx Context,
+    builder: Builder<'ctx>,
+    // named_values: HashMap<String, LLVMValueRef>
+}
 
-// impl<'a> Context<'a> {
-//     pub fn new() -> Context<'a> {
-
-//         let context = Box::new(LLVMContext::create());
-//         let builder = context.create_builder();
-//         // let named_values = HashMap::new();
-//         let typ = context.i32_type();
-
-//         Context { 
-//             context,
-//             builder,
-//             //named_values,
-//             typ,
-//         }
-//     }
-// }
+impl<'ctx> CodeGen<'ctx> {
+    pub fn new(context: &'ctx Context, builder: Builder<'ctx>) -> CodeGen<'ctx> {
+        CodeGen {
+            context,
+            builder,
+        }
+    }
+}
