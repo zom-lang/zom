371c4bb3376404c15e034cd9e213c0087f822c5f
Larsouille25 <superthithi2008@gmail.com>
2023-05-30 20:17:01 +0200

    Rework of the entire lexer, from an Enumerate to a Peekable, add support for comments #14

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::iter::Enumerate;
+use std::iter::Peekable;
 use std::str::Chars;
 
 use crate::error::lexer::IllegalCharError;
@@ -11,7 +11,7 @@ pub struct Lexer<'a> {
     text: String,
     pos: usize, // position in the text
     current_char: Option<char>,
-    iter: Option<Enumerate<Chars<'a>>>,
+    chars: Box<Peekable<Chars<'a>>>,
     line: u32,
     column: usize,
     filename: String,
@@ -23,7 +23,7 @@ impl<'a> Lexer<'a> {
             text: text.to_string(),
             pos: 0,
             current_char: None,
-            iter: None,
+            chars: Box::new(text.chars().peekable()),
             line: 1,
             column: 0,
             filename,
@@ -39,47 +39,67 @@ impl<'a> Lexer<'a> {
 
     pub fn make_tokens(&'a mut self) -> Result<Vec<Token>, Box<dyn Error>> {
         let mut tokens = Vec::new();
+        let mut pos = self.pos;
 
-        self.iter = Some(self.text.chars().enumerate());
+        'main : while let Some(ch) = self.chars.next() {
+            self.pos = pos;
 
-        while let Some((mut _idx, mut _ch)) = self.iter.as_mut().unwrap().next() {
-            self.pos = _idx;
-            match _ch {
-                '0'..='9' | '.' | 'A'..='z' => {
-                    let num = Self::make_word(&self.text, self.pos)?;
-
-                    self.column += num.1 .0;
-
-                    let (tok, new_pos) = num;
-
-                    for _ in 0..(new_pos.0 - 1) {
-                        (_idx, _ch) = self
-                            .iter
-                            .as_mut()
-                            .unwrap()
-                            .next()
-                            .expect("ERR: running out of bounds")
-                    }
-                    tokens.push(tok);
+            match ch {
+                '+' => {
+                    tokens.push(Token::Operator('+'));
+                    pos += 1;
                 }
-                '+' => tokens.push(Token::Operator('+')),
-                '-' => tokens.push(Token::Operator('-')),
-                '*' => tokens.push(Token::Operator('*')),
-                '/' => tokens.push(Token::Operator('/')),
-                '(' => tokens.push(Token::OpenParen),
-                ')' => tokens.push(Token::CloseParen),
-                ';' => tokens.push(Token::Delimiter),
-                ',' => tokens.push(Token::Comma),
+                '-' => {
+                    tokens.push(Token::Operator('-'));
+                    pos += 1;
+                }
+                '*' => {
+                    tokens.push(Token::Operator('*'));
+                    pos += 1;
+                }
+                '/' => {
+                    if let Some('/') = self.chars.peek() {
+                        self.chars.next();
+                        loop {
+                            let ch = self.chars.next();
+                            pos += 1;
+        
+                            if ch == Some('\n') {
+                                continue 'main;
+                            }
+                        }
+                    }
+
+                    tokens.push(Token::Operator('/'));
+                    pos += 1;
+                }
+                '(' => {
+                    tokens.push(Token::OpenParen);
+                    pos += 1;
+                }
+                ')' => {
+                    tokens.push(Token::CloseParen);
+                    pos += 1;
+                }
+                ';' => {
+                    tokens.push(Token::Delimiter);
+                    pos += 1;
+                }
+                ',' => {
+                    tokens.push(Token::Comma);
+                    pos += 1;
+                }
                 '\n' => {
                     self.line += 1;
                     self.column = 0;
                 }
                 _ => {
-                    if _ch.is_whitespace() {
+                    if ch.is_whitespace() {
+                        pos += 1;
                         continue;
                     }
                     return Err(Box::new(IllegalCharError::new(Position::new(
-                        _idx as u32,
+                        pos as u32,
                         self.line,
                         // TODO: Find a way to remove this magic if
                         if self.line == 1 {
@@ -96,73 +116,5 @@ impl<'a> Lexer<'a> {
         }
 
         Ok(tokens)
-    }
-
-    fn set_current_char(text: &str, pos: usize) -> Option<char> {
-        // TODO: rewrite this function I think it's not very efficient ...
-        for (i, c) in text.chars().enumerate() {
-            if i == pos {
-                return Some(c);
-            }
-        }
-        None
-    }
-
-    /// The name of the function isn't very appropriate but this is what this is.
-    /// This function take the text (code) and a position.
-    ///
-    /// This return a Result ->
-    ///     The token, can be if it's only a int or float, a Token::Float or Int with the correct inner value,
-    ///                or if it's not numeric, that returns either Token::Func or Token::Extern if that match with the keyword,
-    ///                or if nothing is "true" that returns a Token::Ident
-    pub fn make_word(text: &str, pos: usize) -> Result<(Token, (usize, char)), Box<dyn Error>> {
-        let mut num_str = String::new();
-        let mut dot_count = 0;
-        let mut pos: usize = pos;
-        let mut curr_char: Option<char> = Self::set_current_char(text, pos);
-        let mut is_numeric = true;
-
-        while let Some(ch) = curr_char {
-            if ch == '.' {
-                dot_count += 1;
-                if dot_count > 1 {
-                    break;
-                }
-            } else if ch.is_whitespace() || !ch.is_alphanumeric() {
-                break;
-            } else if !ch.is_numeric() {
-                is_numeric = false;
-            }
-            num_str.push(ch);
-            pos += 1;
-            curr_char = Self::set_current_char(text, pos);
-        }
-
-        curr_char = Self::set_current_char(text, pos - 1);
-
-        let val = if is_numeric {
-            if dot_count == 0 {
-                Ok((
-                    Token::Int(num_str.parse()?),
-                    (num_str.len(), curr_char.unwrap()),
-                ))
-            } else {
-                Ok((
-                    Token::Float(num_str.parse()?),
-                    (num_str.len(), curr_char.unwrap()),
-                ))
-            }
-        } else {
-            match num_str.as_str() {
-                "func" => Ok((Token::Func, (num_str.len(), curr_char.unwrap()))),
-                "extern" => Ok((Token::Extern, (num_str.len(), curr_char.unwrap()))),
-                _ => Ok((
-                    Token::Ident(num_str.clone()),
-                    (num_str.len(), curr_char.unwrap()),
-                )),
-            }
-        };
-
-        val
     }
 }
