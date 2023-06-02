10a3d42b6a034decb09f3ba4b22246deb86df865
Larsouille25 <superthithi2008@gmail.com>
2023-06-02 23:57:04 +0200

    Add compile_fn and compile_prototype + formatting

--- src/me/compiler.rs
+++ src/me/compiler.rs
@@ -1,16 +1,18 @@
 //! Module related to the transformation of the AST to a LLVM IR.
 
-use std::{collections::HashMap, borrow::Borrow};
+use std::{borrow::Borrow, collections::HashMap};
 
 use inkwell::{
     builder::Builder,
     context::Context,
     module::Module,
     passes::PassManager,
-    values::{FunctionValue, PointerValue, FloatValue, IntValue, BasicMetadataValueEnum}, IntPredicate,
+    types::BasicMetadataTypeEnum,
+    values::{BasicMetadataValueEnum, FloatValue, FunctionValue, IntValue, PointerValue},
+    IntPredicate,
 };
 
-use crate::fe::parser::{Function, Expression};
+use crate::fe::parser::{Expression, Function, Prototype};
 
 /// Defines the `Expression` compiler.
 pub struct Compiler<'a, 'ctx> {
@@ -32,7 +34,7 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
     }
 
     /// Returns the `FunctionValue` representing the function being compiled.
-    /// 
+    ///
     /// Panic:
     ///   Can panic because of the use of `unwrap`
     #[inline]
@@ -60,26 +62,28 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
             Expression::LiteralExpr(nb) => Ok(self.context.i32_type().const_int(nb as u64, true)),
 
             Expression::VariableExpr(ref name) => match self.variables.get(name.as_str()) {
-                Some(var) => Ok(self.builder.build_load(self.context.i32_type(), *var, name.as_str()).into_int_value()),
+                Some(var) => Ok(self
+                    .builder
+                    .build_load(self.context.i32_type(), *var, name.as_str())
+                    .into_int_value()),
                 None => Err("Could not find a matching variable."),
             },
 
-            Expression::BinaryExpr (
-                op,
-                ref left,
-                ref right,
-             ) => {
+            Expression::BinaryExpr(op, ref left, ref right) => {
                 if op == '=' {
                     // handle assignement
                     let var_name = match *left.borrow() {
                         Expression::VariableExpr(ref var_name) => var_name,
                         _ => {
                             return Err("Expected variable as left-hand operator of assignement.");
-                        },
+                        }
                     };
 
                     let var_val = self.compile_expr(right)?;
-                    let var = self.variables.get(var_name.as_str()).ok_or("Undefined variable.")?;
+                    let var = self
+                        .variables
+                        .get(var_name.as_str())
+                        .ok_or("Undefined variable.")?;
 
                     self.builder.build_store(*var, var_val);
 
@@ -94,17 +98,23 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
                         '*' => Ok(self.builder.build_int_mul(lhs, rhs, "tmpmul")),
                         '/' => Ok(self.builder.build_int_signed_div(lhs, rhs, "tmpdiv")),
                         '<' => Ok({
-                            let cmp = self
-                                .builder
-                                .build_int_compare(IntPredicate::ULT, lhs, rhs, "tmpcmp");
+                            let cmp = self.builder.build_int_compare(
+                                IntPredicate::ULT,
+                                lhs,
+                                rhs,
+                                "tmpcmp",
+                            );
 
                             self.builder
                                 .build_int_cast(cmp, self.context.i32_type(), "tmpbool")
                         }),
                         '>' => Ok({
-                            let cmp = self
-                                .builder
-                                .build_int_compare(IntPredicate::ULT, rhs, lhs, "tmpcmp");
+                            let cmp = self.builder.build_int_compare(
+                                IntPredicate::ULT,
+                                rhs,
+                                lhs,
+                                "tmpcmp",
+                            );
 
                             self.builder
                                 .build_int_cast(cmp, self.context.i32_type(), "tmpbool")
@@ -126,38 +136,107 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
                                         Some(value) => Ok(value.into_int_value()),
                                         None => Err("Invalid call produced."),
                                     }
-                                },
+                                }
 
                                 None => Err("Undefined binary operator."),
                             }
-                        },
+                        }
                     }
                 }
-            },
+            }
 
-            Expression::CallExpr ( ref fn_name, ref args ) => match self.get_function(fn_name.as_str()) {
-                Some(fun) => {
-                    let mut compiled_args = Vec::with_capacity(args.len());
+            Expression::CallExpr(ref fn_name, ref args) => {
+                match self.get_function(fn_name.as_str()) {
+                    Some(fun) => {
+                        let mut compiled_args = Vec::with_capacity(args.len());
 
-                    for arg in args {
-                        compiled_args.push(self.compile_expr(arg)?);
+                        for arg in args {
+                            compiled_args.push(self.compile_expr(arg)?);
+                        }
+
+                        let argsv: Vec<BasicMetadataValueEnum> = compiled_args
+                            .iter()
+                            .by_ref()
+                            .map(|&val| val.into())
+                            .collect();
+
+                        match self
+                            .builder
+                            .build_call(fun, argsv.as_slice(), "tmp")
+                            .try_as_basic_value()
+                            .left()
+                        {
+                            Some(value) => Ok(value.into_int_value()),
+                            None => Err("Invalid call produced."),
+                        }
                     }
+                    None => Err("Unknown function."),
+                }
+            }
+        }
+    }
 
-                    let argsv: Vec<BasicMetadataValueEnum> =
-                        compiled_args.iter().by_ref().map(|&val| val.into()).collect();
+    /// Compiles the specified `Prototype` into an extern LLVM `FunctionValue`.
+    fn compile_prototype(&self, proto: &Prototype) -> Result<FunctionValue<'ctx>, &'static str> {
+        let ret_type = self.context.i32_type();
+        let args_types = std::iter::repeat(ret_type)
+            .take(proto.args.len())
+            .map(|f| f.into())
+            .collect::<Vec<BasicMetadataTypeEnum>>();
+        let args_types = args_types.as_slice();
 
-                    match self
-                        .builder
-                        .build_call(fun, argsv.as_slice(), "tmp")
-                        .try_as_basic_value()
-                        .left()
-                    {
-                        Some(value) => Ok(value.into_int_value()),
-                        None => Err("Invalid call produced."),
-                    }
-                },
-                None => Err("Unknown function."),
-            },
+        let fn_type = self.context.i32_type().fn_type(args_types, false);
+        let fn_val = self.module.add_function(proto.name.as_str(), fn_type, None);
+
+        // set arguments names
+        for (i, arg) in fn_val.get_param_iter().enumerate() {
+            arg.set_name(proto.args[i].as_str());
+        }
+
+        // finally return built prototype
+        Ok(fn_val)
+    }
+
+    /// Compiles the specified `Function` into an LLVM `FunctionValue`.
+    fn compile_fn(&mut self) -> Result<FunctionValue<'ctx>, &'static str> {
+        let proto = &self.function.prototype;
+        let function = self.compile_prototype(proto)?;
+
+        let entry = self.context.append_basic_block(function, "entry");
+
+        self.builder.position_at_end(entry);
+
+        // update fn field
+        self.fn_value_opt = Some(function);
+
+        // build variables map
+        self.variables.reserve(proto.args.len());
+
+        for (i, arg) in function.get_param_iter().enumerate() {
+            let arg_name = proto.args[i].as_str();
+            let alloca = self.create_entry_block_alloca(arg_name);
+
+            self.builder.build_store(alloca, arg);
+
+            self.variables.insert(proto.args[i].clone(), alloca);
+        }
+
+        // compile body
+        let body = self.compile_expr(&self.function.body)?;
+
+        self.builder.build_return(Some(&body));
+
+        // return the whole thing after verification and optimization
+        if function.verify(true) {
+            self.fpm.run_on(&function);
+
+            Ok(function)
+        } else {
+            unsafe {
+                function.delete();
+            }
+
+            Err("Invalid generated function.")
         }
     }
 }
