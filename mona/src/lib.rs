f633879dfc876fc8e22c7e0645f4362cb0ba820b
Larsouille25 <superthithi2008@gmail.com>
2023-06-05 12:38:11 +0200

    Adding doc to the lib of mona crate

---
+++ mona/src/lib.rs
@@ -0,0 +1,8 @@
+//! Mona is a AOT compiled programming language. The binary implements a REPL and soon; a file reader.
+//!
+//! The front-end (lexer, parser, token, ast) is in the crate `mona_fe`.
+//!
+//! Custom errors are in module `error` in `src/error/`.
+//! A REPL for Mona is in the `driver` module in `src/driver.rs`.
+//!
+//! Mona repository link -> <https://github.com/Larsouille25/mona>
