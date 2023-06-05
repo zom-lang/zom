c08b1e229dd7417285350bc293b6da8cc9058c65
Larsouille25 <superthithi2008@gmail.com>
2023-06-05 16:12:28 +0200

    Add all basis for the Mona CLI Command

--- mona/src/lib.rs
+++ mona/src/lib.rs
@@ -6,3 +6,50 @@
 //! A REPL for Mona is in the `driver` module in `src/driver.rs`.
 //!
 //! Mona repository link -> <https://github.com/Larsouille25/mona>
+
+mod ops;
+
+use std::ffi::OsString;
+
+use clap::{Parser, Subcommand};
+use ops::bobj;
+
+#[derive(Parser)]
+#[clap()]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Builds a given file into an object file
+    Bobj(bobj::Args),
+}
+
+#[derive(Copy, Debug, Clone, PartialEq, Eq)]
+pub enum ExitStatus {
+    Success,
+    Error,
+}
+
+impl From<bool> for ExitStatus {
+    fn from(value: bool) -> Self {
+        if value {
+            ExitStatus::Success
+        } else {
+            ExitStatus::Error
+        }
+    }
+}
+
+pub fn run_with_args<T, I>(args: I) -> Result<ExitStatus, anyhow::Error>
+where
+    I: IntoIterator<Item = T>,
+    T: Into<OsString> + Clone,
+{
+    let args = Args::parse_from(args);
+    match args.command {
+        Command::Bobj(args) => bobj::build(args),
+    }
+}
---
+++ mona/src/ops/bobj.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+
+use crate::ExitStatus;
+
+
+#[derive(clap::Args)]
+pub struct Args {
+    /// Path to the Mona source file 
+    file_path: PathBuf,
+
+    /// Path to where the object file will go
+    #[clap(short, long)]
+    output_file: Option<PathBuf>,
+
+    /// Path to where the object file will go
+    #[clap(short = 'O', long, default_value_t = 2)] // TODO: Change this to the actual things later.
+    optimization_level: usize, 
+
+    /// Emits IR instead of a *.o
+    #[clap(long, short)]
+    emit_ir: Option<bool>,
+}
+
+pub fn build(args: Args) -> Result<ExitStatus, anyhow::Error> {
+    todo!()
+}
