3fee9c7c3f78ae7e38d0cb6d83a36dc0f6022c57
Larsouille25 <superthithi2008@gmail.com>
2023-06-06 16:48:22 +0200

    fix: the function get_default_triple() is less weird

--- mona/src/ops/gettarget.rs
+++ mona/src/ops/gettarget.rs
@@ -4,11 +4,7 @@ use mona_compiler::target::get_target_triple;
 
 pub fn gettarget() -> Result<ExitStatus, anyhow::Error> {
     // Yes, it's not a very good implementation but ...
-    let mut res = get_target_triple()[14..].to_owned();
-    res.pop();
-    res.pop();
-
-    println!("Target: {}", res);
+    println!("Target: {}", get_target_triple());
 
     Ok(ExitStatus::Success)
 }
--- mona_compiler/src/target.rs
+++ mona_compiler/src/target.rs
@@ -1,6 +1,12 @@
 pub use inkwell::targets::TargetMachine;
 
 /// This function is an abstraction for the `mona` bin.
+#[inline]
 pub fn get_target_triple() -> String {
-    TargetMachine::get_default_triple().to_string()
+    TargetMachine::get_default_triple()
+        .as_str()
+        .to_owned()
+        .to_str()
+        .expect("Error while trying to get the target trimple to a &str.")
+        .to_owned()
 }
