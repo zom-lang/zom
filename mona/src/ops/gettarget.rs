5b2ee360fa9259dadddc4da455be7560fc535cb7
Larsouille25 <superthithi2008@gmail.com>
2023-06-06 12:24:54 +0200

    feat(mona.get-target): Implementing the command

--- mona/src/ops/gettarget.rs
+++ mona/src/ops/gettarget.rs
@@ -1,5 +1,14 @@
 use crate::ExitStatus;
 
+use mona_compiler::target::get_target_triple;
+
 pub fn gettarget() -> Result<ExitStatus, anyhow::Error> {
-    todo!();
+    // Yes, it's not a very good implementation but ...
+    let mut res = get_target_triple()[14..].to_owned();
+    res.pop();
+    res.pop();
+
+    println!("Target: {}", res);
+
+    Ok(ExitStatus::Success)
 }
