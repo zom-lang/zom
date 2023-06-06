c9c773939a7fc3269825c536f719553f8482e30e
Larsouille25 <superthithi2008@gmail.com>
2023-06-06 11:20:39 +0200

    feat(mona.version): print if its a debug binary

--- mona/src/ops/version.rs
+++ mona/src/ops/version.rs
@@ -1,11 +1,16 @@
 use crate::ExitStatus;
 
-
 pub fn version() -> Result<ExitStatus, anyhow::Error> {
-    println!(
-        "Mona v{}",
+    print!(
+        "Mona v{} ",
         env!("CARGO_PKG_VERSION")
     );
+
+    cfg!(debug_assertions).then(|| {
+        print!("in DEBUG binary")
+    });
+
+    println!();
 
     Ok(ExitStatus::Success)
 }
