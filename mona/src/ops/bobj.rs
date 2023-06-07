856eb2398fb1d7df59e57d4596d7c328a1a65ab2
Larsouille25 <superthithi2008@gmail.com>
2023-06-07 12:45:47 +0200

    feat(mona.bobj): full implementation of Build Object subcommand

--- mona/src/ops/bobj.rs
+++ mona/src/ops/bobj.rs
@@ -1,7 +1,10 @@
-use std::{path::PathBuf, fs, mem};
+use std::{path::PathBuf, fs::{self, File}, mem, io::Write};
 
 use anyhow::anyhow;
-use mona_fe::lexer::Lexer;
+use inkwell::{context::Context, passes::PassManager, values::AnyValue};
+use mona_codegen::gen::CodeGen;
+use mona_compiler::compiler::Compiler;
+use mona_fe::{lexer::Lexer, parser::{parse, ParserSettings}};
 
 use crate::ExitStatus;
 
@@ -45,7 +48,6 @@ pub fn build(mut args: Args) -> Result<ExitStatus, anyhow::Error> {
 
     println!("after the lexer");
 
-    // TODO: DOESN'T WORK.
     let tokens = match lexer.make_tokens() {
         Ok(src) => src,
         Err(_) => return Err(anyhow!("Error while trying to read the source file.")),
@@ -53,6 +55,73 @@ pub fn build(mut args: Args) -> Result<ExitStatus, anyhow::Error> {
 
     println!("{:?}", tokens);
 
+    let parse_result = parse(tokens.as_slice(), &[], &mut ParserSettings::default());
+
+    let ast;
+
+    match parse_result {
+        Ok((parsed_ast, rest)) => {
+            if rest.is_empty() {
+                ast = parsed_ast;
+            }else {
+                return Err(anyhow!("There is rest after parsing."))
+            }
+        }
+        Err(_) => return Err(anyhow!("Parsing error occurs."))
+    }
+
+    println!("{:#?}", ast);
+
+    let context = Context::create();
+    let module = context.create_module("repl");
+    let builder = context.create_builder();
+
+    // Create FPM
+    let fpm = PassManager::create(&module);
+
+    fpm.add_instruction_combining_pass();
+    fpm.add_reassociate_pass();
+    fpm.add_gvn_pass();
+    fpm.add_cfg_simplification_pass();
+    fpm.add_basic_alias_analysis_pass();
+    fpm.add_promote_memory_to_register_pass();
+    fpm.add_instruction_combining_pass();
+    fpm.add_reassociate_pass();
+
+    fpm.initialize();
+
+    let module = context.create_module(mem::take(&mut args.source_file.as_mut_os_str().to_str().unwrap()));
+
+    let compile_res = CodeGen::compile_ast(&context, &builder, &fpm, &module, &ast);
+
+    match compile_res {
+        Ok(funcs) => {
+            if args.emit_ir {
+                for fun in funcs {
+                    let str = fun.print_to_string();
+                    match args.output_file {
+                        Some(ref path) => {
+                            let mut file = File::create(path).expect("Couldn't open the file");
+                            file.write(str.to_bytes()).expect("Could write to the file");
+                        }
+                        None => return Err(anyhow!("Couldn't unwrap the file path"))
+                    }
+                }
+            }
+            else {
+                match args.output_file {
+                    Some(ref path) => {
+                        Compiler::compile_default(module, path).expect("Couldn't compile to object file")
+                    }
+                    None => return Err(anyhow!("Couldn't unwrap the file path"))
+                }
+            }
+        }
+        Err(err) => {
+            println!("{err}");
+            return Err(anyhow!("Error was occur when trying to generate the code"))
+        }
+    }
 
     Ok(ExitStatus::Success)
 }
