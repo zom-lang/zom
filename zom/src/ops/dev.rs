cfc42fa642f8e3170980c39982c5ec4133dd9b00
Larsouille25 <superthithi2008@gmail.com>
2023-06-29 13:17:04 +0200

    chore: add parsing to the dev subcommand

--- zom/src/ops/dev.rs
+++ zom/src/ops/dev.rs
@@ -1,6 +1,7 @@
 use std::io::{self, stdout, Write};
 use anyhow::anyhow;
 use zom_fe::lexer::Lexer;
+use zom_fe::parser::{parse, ParserSettings, ParsingContext};
 
 use crate::ExitStatus;
 
@@ -19,9 +20,27 @@ pub fn dev() -> Result<ExitStatus, anyhow::Error> {
     println!("buffer = {:?}", buffer);
 
     let mut lexer = Lexer::new(buffer.as_str(), "<dev_cmd>.zom".to_string());
-
+    let tokens;
     match lexer.make_tokens() {
-        Ok(toks) => println!("toks = {:?}", toks),
+        Ok(toks) => { 
+            tokens = toks;
+            println!("toks = {:?}", tokens);
+        },
+        Err(err) => return Err(anyhow!(format!("{}", err)))
+    }
+
+    let mut parse_context = ParsingContext::new(
+        "<dev_cmd>.zom".to_owned(), 
+        buffer
+    );
+
+    let ast_result = parse(tokens.as_slice(), &[], &mut ParserSettings::default(), &mut parse_context);
+
+    match ast_result {
+        Ok((ast, rest_toks)) => {
+            println!("ast = {:#?}", ast);
+            println!("toks_rest = {:?}", rest_toks);
+        }
         Err(err) => return Err(anyhow!(format!("{}", err)))
     }
 
