9614231b515d21478823d3d7b6b412f9216a025c
Larsouille25 <superthithi2008@gmail.com>
2023-07-15 17:03:53 +0200

    feat: re-enable lexing for Multi line comments, single line comments, operators, number literal, keyword and identifier

--- zom_fe/src/lexer.rs
+++ zom_fe/src/lexer.rs
@@ -66,84 +66,84 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn make_tokens(&'a mut self) -> Result<Vec<Token>, Box<dyn Error>> {
-        let mut tokens = Vec::new();
+        let mut tokens: Vec<Token> = Vec::new();
 
         'main: while let Some(ch) = self.chars.next() {
             match ch {
-                // '0'..='9' | 'A'..='Z' | 'a'..='z' | '_' => {
-                //     tokens.push(self.lex_lki(ch)?);
-                // }
-                // ch if is_start_operator(ch) => {
-                //     let window = &self.text.get(self.pos..self.pos + OP_MAX_LENGHT);
+                '0'..='9' | 'A'..='Z' | 'a'..='z' | '_' => {
+                    let old_pos = self.pos;
+                    tokens.push(Token::new(self.lex_lki(ch)?, old_pos..=(self.pos - 1)));
+                }
+                ch if is_start_operator(ch) => {
+                    let window = &self.text.get(self.pos..self.pos + OP_MAX_LENGHT);
 
-                //     if window.is_none() {
-                //         continue;
-                //     }
+                    if window.is_none() {
+                        continue;
+                    }
 
-                //     let window = window.unwrap();
-                //     let (is_op, len) = is_operator(window);
+                    let window = window.unwrap();
+                    let (is_op, len) = is_operator(window);
 
-                //     if is_op {
-                //         tokens.push(Operator(window[..len].to_owned()));
-                //         self.pos += len;
-                //         self.column += len;
-                //         continue;
-                //     }
-                // }
-                // '#' => {
-                //     self.chars.next();
-                //     loop {
-                //         let ch = self.chars.next();
-                //         self.incr_pos();
+                    if is_op {
+                        tokens.push(Token::new(Operator(window[..len].to_owned()), self.pos..=(self.pos + len - 1)));
+                        self.pos += len;
+                        self.column += len;
+                        continue;
+                    }
+                }
+                '#' => {
+                    loop {
+                        let ch = self.chars.next();
+                        self.incr_pos();
 
-                //         if ch == Some('\n') {
-                //             continue 'main;
-                //         }
-                //     }
-                // }
-                // '(' => {
-                //     if let Some('*') = self.chars.peek() {
-                //         // Eat the `*` char
-                //         self.chars.next();
-                //         self.incr_pos();
-                //         let mut comment = String::new();
+                        if ch == Some('\n') {
+                            continue 'main;
+                        }
+                    }
+                }
+                '(' => {
+                    if let Some('*') = self.chars.peek() {
+                        // Eat the `*` char
+                        self.chars.next();
+                        self.incr_pos();
+                        let mut comment = String::new();
 
-                //         'comment: loop {
-                //             let ch = self.chars.next();
+                        'comment: loop {
+                            let ch = self.chars.next();
 
-                //             if ch == Some('\n') {
-                //                 self.line += 1;
-                //                 self.column = 0;
-                //                 self.pos += 1;
-                //                 continue 'comment;
-                //             } else {
-                //                 self.incr_pos();
-                //             }
+                            if ch == Some('\n') {
+                                self.line += 1;
+                                self.column = 0;
+                                self.pos += 1;
+                                continue 'comment;
+                            } else {
+                                self.incr_pos();
+                            }
 
-                //             let window = &self.text.get(self.pos..self.pos + 2);
+                            let window = &self.text.get(self.pos..self.pos + 2);
 
-                //             if ch.is_none() {
-                //                 break 'comment;
-                //             }
+                            if ch.is_none() {
+                                break 'comment;
+                            }
 
-                //             if window.is_none() {
-                //                 continue;
-                //             }
-                //             let window = window.unwrap();
+                            if window.is_none() {
+                                continue;
+                            }
+                            let window = window.unwrap();
 
-                //             if window == "*)" {
-                //                 self.incr_pos();
-                //                 break 'comment;
-                //             }
-                //             comment.push(ch.unwrap());
-                //         }
-                //         self.chars.next();
-                //         self.incr_pos();
-                //         continue 'main;
-                //     }
-                //     tokens.push(Token::OpenParen);
-                //     self.incr_pos();
-                // }
+                            if window == "*)" {
+                                self.incr_pos();
+                                break 'comment;
+                            }
+                            comment.push(ch.unwrap());
+                        }
+                        self.chars.next();
+                        self.incr_pos();
+                        continue 'main;
+                    }
+                    tokens.push(Token::new(OpenParen, self.pos..=self.pos));
+                    self.incr_pos();
+                }
                 ')' => self.match_arm(&mut tokens, CloseParen),
                 '[' => self.match_arm(&mut tokens, OpenBracket),
                 ']' => self.match_arm(&mut tokens, CloseBracket),
@@ -187,7 +187,7 @@ impl<'a> Lexer<'a> {
     ///     text: `test` -> Ident("est")
     /// And after it is like that :
     ///     text: `test` -> Ident("test")
-    fn lex_lki(&mut self, ch: char) -> Result<Token, Box<dyn Error>> {
+    fn lex_lki(&mut self, ch: char) -> Result<TokenType, Box<dyn Error>> {
         let mut num_str = String::new();
         let mut dot_count = 0;
         let mut is_numeric = true;
@@ -221,29 +221,29 @@ impl<'a> Lexer<'a> {
                 }
             }
         }
-        todo!()
-        // if is_numeric {
-        //     if dot_count == 0 {
-        //         Ok(Token::Int(num_str.parse()?))
-        //     } else {
-        //         Ok(Token::Float(num_str.parse()?))
-        //     }
-        // } else {
-        //     match num_str.as_str() {
-        //         KEY_FUNC => Ok(Token::Func),
-        //         KEY_EXTERN => Ok(Token::Extern),
-        //         KEY_VAR => Ok(Token::Var),
-        //         KEY_CONST => Ok(Token::Const),
-        //         KEY_STRUCT => Ok(Token::Struct),
-        //         KEY_ENUM => Ok(Token::Enum),
-        //         KEY_RETURN => Ok(Token::Return),
-        //         KEY_IF => Ok(Token::If),
-        //         KEY_ELSE => Ok(Token::Else),
-        //         KEY_WHILE => Ok(Token::While),
-        //         KEY_FOR => Ok(Token::For),
-        //         KEY_PUB => Ok(Token::Pub),
-        //         _ => Ok(Token::Ident(num_str.clone())),
-        //     }
-        // }
+
+        if is_numeric {
+            if dot_count == 0 {
+                Ok(Int(num_str.parse()?))
+            } else {
+                Ok(Float(num_str.parse()?))
+            }
+        } else {
+            match num_str.as_str() {
+                KEY_FUNC => Ok(Func),
+                KEY_EXTERN => Ok(Extern),
+                KEY_VAR => Ok(Var),
+                KEY_CONST => Ok(Const),
+                KEY_STRUCT => Ok(Struct),
+                KEY_ENUM => Ok(Enum),
+                KEY_RETURN => Ok(Return),
+                KEY_IF => Ok(If),
+                KEY_ELSE => Ok(Else),
+                KEY_WHILE => Ok(While),
+                KEY_FOR => Ok(For),
+                KEY_PUB => Ok(Pub),
+                _ => Ok(Ident(num_str.clone())),
+            }
+        }
     }
 }
